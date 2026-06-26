69d4ca792858e9798bf594051dce457adbf1f2ed
mohansvapps-eng <mohansvapps@gmail.com>
2026-06-26 14:38:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Thank you for contacting Web Design Company! We'll get back to you soon.");
+});
